c5088f37471a010df1a949743466bec37e34c1d6
marcinpk <marcinpk.mp@gmail.com>
2019-04-02 14:43:43 +0200

    Wyznaczanie zmiennych dla problemu CSP

--- Data.py
+++ Data.py
@@ -5,9 +5,10 @@ def load_data(file_name):
     with open(file_name) as file:
         dimension = int(file.readline())
         board_matrix = load_board_matrix(file, dimension)
+        variables_arr = make_variables(board_matrix)
         constraint_arr = load_constraints_array(file)
 
-    return Data(dimension, board_matrix, constraint_arr)
+    return Data(dimension, board_matrix, constraint_arr, variables_arr)
 
 
 def load_board_matrix(file, dimension):
@@ -23,6 +24,21 @@ def load_board_matrix(file, dimension):
         line_number += 1
 
     return board_matrix
+
+
+def make_variables(board_matrix):
+    zeros_positions_arr = np.where(board_matrix == 0)
+    print(zeros_positions_arr)
+    variables_count = zeros_positions_arr[0].size
+    dimension = board_matrix[0].size
+    variables_arr = np.empty(shape=(variables_count, dimension + 2), dtype=int)
+    for i in range(variables_count):
+        field_arr = np.arange(1, dimension+1)
+        coord_arr = np.array((zeros_positions_arr[0][i], zeros_positions_arr[1][i]))
+        variable = np.concatenate((field_arr, coord_arr))
+        variables_arr[i] = variable
+
+    return variables_arr
 
 
 def load_constraints_array(file):
@@ -44,14 +60,14 @@ def make_constraint(constraint_string):
 
 
 def fix_constraint(constraint_string):
-    return constraint_string.replace("1", "0").replace("2", "1").replace("3", "2").replace("4", "3")\
+    return constraint_string.replace("1", "0").replace("2", "1").replace("3", "2").replace("4", "3") \
         .replace("A", "0").replace("B", "1").replace("C", "2").replace("D", "3")
-
 
 
 class Data:
 
-    def __init__(self, dimensions, board_matrix, constraints_arr):
-        self.dimensions = dimensions
+    def __init__(self, dimension, board_matrix, constraints_arr, variables_arr):
+        self.dimension = dimension
         self.board_matrix = board_matrix
         self.constraints_arr = constraints_arr
+        self.variables_arr = variables_arr
--- Main.py
+++ Main.py
@@ -1,11 +1,33 @@
+import numpy as np
 from Data import load_data
 
-if __name__ == '__main__':
-    print("start main")
+FILE_NAME = "futoshiki_4_0.txt"
 
-    data = load_data("futoshiki_4_1.txt")
 
+def start():
+    print("start")
+    data = load_data(FILE_NAME)
     print("data matrix")
     print(data.board_matrix)
     print("constraints")
     print(data.constraints_arr)
+    print("variables")
+    print(data.variables_arr)
+    search_solution(data)
+
+
+def search_solution(data):
+    dimension = int(data.dimension)
+    variables_arr = np.copy(data.variables_arr)
+    board_matrix = np.copy(data.board_matrix)
+    constraints_arr = np.copy(data.constraints_arr)
+
+    curr_var_idx = 0
+    for idx in range(variables_arr.size):
+        variable = np.copy(variables_arr[curr_var_idx])
+        coords_arr = variable[dimension:]
+        field_arr = variable[:dimension]
+
+
+if __name__ == '__main__':
+    start()
